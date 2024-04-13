ff176d38eef608ea7ffa5c9b7a61ae34235e0ed8
DOrtenzio <dortenzio.diego.studente@itispaleocapa.it>
2024-04-13 17:17:04 +0200

    ricercaLineare

--- src/utili/Vettori.java
+++ src/utili/Vettori.java
@@ -24,5 +24,14 @@ public class Vettori {
             a[posmax]=temp;
         }
     }
-
+    public static int ricercaLineare ( int [] a, int elemento){
+        int pos=-1;
+        for (int i=0;i<a.length;i++){
+            if (a[i]==elemento){
+                pos=i;
+                break;
+            }
+        }
+        return pos;
+    }
 }
